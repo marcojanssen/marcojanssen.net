277ccc80bce3583ab6d427c367128cba45dbabbb
Marco Janssen <dev@marcojanssen.net>
2015-03-04 23:29:59 +0100

    Added gh-pages deployment

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-rm -rf public/
+rm -rf deploy/
 
-git clone git@github.com:marcojanssen/marcojanssen.github.io.git public
+git clone git@github.com:marcojanssen/marcojanssen.net deploy --branch gh-pages
 
-hugo -b http://marcojanssen.net
+hugo --baseUrl=http://marcojanssen.net --destination=deploy
 
-cd public
+cd deploy
 
 git add --all
 git commit -m "$1"
-git push origin master
+git push origin gh-pages
 
 cd ../
